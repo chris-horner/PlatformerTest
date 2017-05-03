52fc848910f342b8e810458b1c256b0f6e85c1d2
Christopher Horner <horner.cd@gmail.com>
2017-05-03 23:25:52 +1000

    E06 Moving platform basics.

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -1,25 +1,10 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
-public class Controller2D : MonoBehaviour {
-  private const float SKIN_WIDTH = 0.015f;
+public class Controller2D : RaycastController {
   private const float MAX_CLIMB_ANGLE = 80f;
-  private const float MAX_DESCEND_ANGLE = 75f;
+  private const float MAX_DESCEND_ANGLE = 80f;
 
-  public LayerMask collisionMask;
-  public int horizontalRayCount = 4;
-  public int verticalRayCount = 4;
   public CollisionInfo collisions;
-
-  private float horizontalRaySpacing;
-  private float verticalRaySpacing;
-  private new BoxCollider2D collider;
-  private RaycastOrigins raycastOrigins;
-
-  private void Start() {
-    collider = GetComponent<BoxCollider2D>();
-    CalculateRaySpacing();
-  }
 
   public void Move(Vector3 velocity) {
     UpdateRaycastOrigins();
@@ -84,7 +69,7 @@ public class Controller2D : MonoBehaviour {
     float rayLength = Mathf.Abs(velocity.y) + SKIN_WIDTH;
 
     for (int i = 0; i < verticalRayCount; i++) {
-      Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+      Vector2 rayOrigin = directionY == -1 ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
       rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
       RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
 
@@ -157,34 +142,6 @@ public class Controller2D : MonoBehaviour {
         }
       }
     }
-  }
-
-  private void UpdateRaycastOrigins() {
-    Bounds bounds = collider.bounds;
-    bounds.Expand(SKIN_WIDTH * -2);
-
-    raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
-    raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
-    raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
-    raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
-  }
-
-  private void CalculateRaySpacing() {
-    Bounds bounds = collider.bounds;
-    bounds.Expand(SKIN_WIDTH * -2);
-
-    horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
-    verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
-
-    horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
-    verticalRaySpacing = bounds.size.y / (verticalRayCount - 1);
-  }
-
-  private struct RaycastOrigins {
-    public Vector2 topLeft;
-    public Vector2 topRight;
-    public Vector2 bottomLeft;
-    public Vector2 bottomRight;
   }
 
   public struct CollisionInfo {
---
+++ Assets/Scripts/RaycastController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RaycastController : MonoBehaviour {
+  protected const float SKIN_WIDTH = 0.015f;
+
+  protected float horizontalRaySpacing;
+  protected float verticalRaySpacing;
+  protected new BoxCollider2D collider;
+  protected RaycastOrigins raycastOrigins;
+
+  public LayerMask collisionMask;
+  public int horizontalRayCount = 4;
+  public int verticalRayCount = 4;
+
+  protected virtual void Start() {
+    collider = GetComponent<BoxCollider2D>();
+    CalculateRaySpacing();
+  }
+
+  protected void UpdateRaycastOrigins() {
+    Bounds bounds = collider.bounds;
+    bounds.Expand(SKIN_WIDTH * -2);
+
+    raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+    raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+    raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+    raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+  }
+
+  protected void CalculateRaySpacing() {
+    Bounds bounds = collider.bounds;
+    bounds.Expand(SKIN_WIDTH * -2);
+
+    horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+    verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+    horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+    verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+  }
+
+  protected struct RaycastOrigins {
+    public Vector2 topLeft;
+    public Vector2 topRight;
+    public Vector2 bottomLeft;
+    public Vector2 bottomRight;
+  }
+}
